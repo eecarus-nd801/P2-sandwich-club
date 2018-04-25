9a67dbc4459eac82c605ec214c8af774bb4f2e10
eecarus <eecarus@hotmail.com>
2018-04-25 11:21:40 -0400

    got first working version,padding is a bit off right now

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,34 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private ImageView mImageIv;
+    private TextView mAlsoKnownAsTv;
+    private TextView mDescriptionTv;
+    private TextView mIngredientsTv;
+    private TextView mPlaceOfOriginTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        initializeViewReferences();
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +53,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(mImageIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +66,28 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void initializeViewReferences() {
+        mImageIv = findViewById(R.id.image_iv);
+        mAlsoKnownAsTv = findViewById(R.id.also_known_tv);
+        mDescriptionTv = findViewById(R.id.description_tv);
+        mIngredientsTv = findViewById(R.id.ingredients_tv);
+        mPlaceOfOriginTv = findViewById(R.id.origin_tv);
+    }
 
+    private void populateUI(Sandwich sandwich) {
+        mAlsoKnownAsTv.setText(joinListWithNewlineSeparator(sandwich.getAlsoKnownAs()));
+        mDescriptionTv.setText(defaultString(sandwich.getDescription()));
+        mIngredientsTv.setText(joinListWithNewlineSeparator(sandwich.getIngredients()));
+        mPlaceOfOriginTv.setText(defaultString(sandwich.getPlaceOfOrigin()));
+    }
+
+    private String joinListWithNewlineSeparator(List<String> listOfStrings) {
+        if (listOfStrings == null || listOfStrings.isEmpty())
+            return "";
+        return TextUtils.join("\n", listOfStrings);
+    }
+
+    private String defaultString(String original) {
+        return TextUtils.isEmpty(original) ? "" : original;
     }
 }
